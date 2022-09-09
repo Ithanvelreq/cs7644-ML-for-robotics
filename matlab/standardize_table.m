edbae68c7a3f454d1ce64bd6785368d04a0bf963
Ithan <ivelarde@msad.georgiatech-metz.fr>
2022-09-09 15:16:35 +0200

    standardization works

--- matlab/standardize_table.m
+++
@@ -1,8 +0,0 @@
-function [table] = standardize_table(table, name)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-table.x_time = table.x_time / 1e9;
-table.x_time = table.x_time - table.x_time(1);
-writetable(table, name, 'Delimiter', ',')
-end
-
