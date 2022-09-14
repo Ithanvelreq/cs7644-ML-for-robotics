2b40c21c19e094ff5b995ed114c804b2c3373946
Ithan <ivelarde@gtlpc109.georgiatech-metz.fr>
2022-09-14 10:57:48 +0200

    remap bug fixed

--- src/hw0_velarde/src/teleop_joy.cpp
+++ src/hw0_velarde/src/teleop_joy.cpp
@@ -35,7 +35,9 @@ TeloepJoy::TeloepJoy():
 
 
   //vel_pub_ = nh_.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
-  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/sim_ros_interface/drone/cmd_vel", 1);
+  //vel_pub_ = nh_.advertise<geometry_msgs::Twist>("vrep/twistCommand", 1);
+  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("joystic/cmd", 1);
+  //vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/sim_ros_interface/drone/cmd_vel", 1);
 
 
   joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeloepJoy::joyCallback, this);
